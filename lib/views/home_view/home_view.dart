5b81777b5bfa25f416187b2964ad8d9f51909fae
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2023-11-05 14:59:55 +0300

    update 🆕: widget

--- lib/views/home_view/home_view.dart
+++ lib/views/home_view/home_view.dart
@@ -1,5 +1,6 @@
-
 import 'package:calculator_app/views/repository_view/bloc/calculator_bloc.dart';
+import 'package:calculator_app/views/widgets/buttons_widget.dart';
+import 'package:calculator_app/views/widgets/text_field_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -19,128 +20,46 @@ class HomeView extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children:[
-                const SizedBox(height: 20.0),
-                TextField(
-                  controller: _controllerFirstNum,
-                  decoration: const InputDecoration(
-                      labelText: 'First Number',
-                      hintText: 'Enter First Number'),
-                  style: const TextStyle(fontSize: 20),
-                  keyboardType: TextInputType.number,
-                ),
-                TextField(
-                  controller: _controllerSecondNum,
-                  decoration: const InputDecoration(
-                      labelText: 'Second Number',
-                      hintText: 'Enter Second Number'),
-                  style: const TextStyle(fontSize: 20),
-                  keyboardType: TextInputType.number,
-                ),
-                const SizedBox(height: 10.0),
-                Row(
-                  children: [
-                    Expanded(
-                      child: ElevatedButton(
-                        child: const Text(
-                          '+',
-                          style: TextStyle(
-                              fontSize: 22, fontWeight: FontWeight.bold),
-                        ),
-                        onPressed: () {
-                          if (_controllerFirstNum.text.isNotEmpty &&
-                              _controllerSecondNum.text.isNotEmpty) {
-                            num num1 = num.parse(_controllerFirstNum.text);
-                            num num2 = num.parse(_controllerSecondNum.text);
-                            calculatorBloc
-                                .add(AddEvent(num1: num1, num2: num2));
-                          }
-                        },
-                      ),
-                    ),
-                    const SizedBox(width: 8.0),
-                    Expanded(
-                      child: ElevatedButton(
-                        child: const Text(
-                          '-',
-                          style: TextStyle(
-                              fontSize: 22, fontWeight: FontWeight.bold),
-                        ),
-                        onPressed: () {
-                          if (_controllerFirstNum.text.isNotEmpty &&
-                              _controllerSecondNum.text.isNotEmpty) {
-                            num num1 = num.parse(_controllerFirstNum.text);
-                            num num2 = num.parse(_controllerSecondNum.text);
-                            calculatorBloc
-                                .add(SubtractEvent(num1: num1, num2: num2));
-                          }
-                        },
-                      ),
-                    ),
-                    const SizedBox(width: 8.0),
-                    Expanded(
-                      child: ElevatedButton(
-                        child: const Text(
-                          'x',
-                          style: TextStyle(
-                              fontSize: 22, fontWeight: FontWeight.bold),
-                        ),
-                        onPressed: () {
-                          if (_controllerFirstNum.text.isNotEmpty &&
-                              _controllerSecondNum.text.isNotEmpty) {
-                            num num1 = num.parse(_controllerFirstNum.text);
-                            num num2 = num.parse(_controllerSecondNum.text);
-                            calculatorBloc
-                                .add(MultiplyEvent(num1: num1, num2: num2));
-                          }
-                        },
-                      ),
-                    ),
-                    const SizedBox(width: 8.0),
-                    Expanded(
-                      child: ElevatedButton(
-                        child: const Text(
-                          '/',
-                          style: TextStyle(
-                              fontSize: 22, fontWeight: FontWeight.bold),
-                        ),
-                        onPressed: () {
-                          if (_controllerFirstNum.text.isNotEmpty &&
-                              _controllerSecondNum.text.isNotEmpty) {
-                            num num1 = num.parse(_controllerFirstNum.text);
-                            num num2 = num.parse(_controllerSecondNum.text);
-                            calculatorBloc
-                                .add(DivideEvent(num1: num1, num2: num2));
-                          }
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20.0),
-                BlocBuilder(
-                  bloc: calculatorBloc,
-                  builder: (context, state) {
-                    if (state is CalculatorResult) {
-                      return Text(
-                        'Result: ${state.result.toStringAsPrecision(3)}',
-                        style: const TextStyle(
-                            fontSize: 22, fontWeight: FontWeight.bold),
-                      );
-                    } else {
-                      return const Text(
-                        'Something went wrong',
-                        style: TextStyle(
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.red),
-                      );
-                    }
-                  },
-                ),
-              ]),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+            const SizedBox(height: 20.0),
+            TextFieldWidget(
+              controllerFirstNum: _controllerFirstNum,
+              text1: 'First Number',
+              text2: 'Enter First Number',
+            ),
+            TextFieldWidget(
+              controllerFirstNum: _controllerFirstNum,
+              text1: 'Second Number',
+              text2: 'Enter Second Number',
+            ),
+            const SizedBox(height: 10.0),
+            ButtonsWidget(
+                controllerFirstNum: _controllerFirstNum,
+                controllerSecondNum: _controllerSecondNum,
+                calculatorBloc: calculatorBloc),
+            const SizedBox(height: 20.0),
+            BlocBuilder(
+              bloc: calculatorBloc,
+              builder: (context, state) {
+                if (state is CalculatorResult) {
+                  return Text(
+                    'Result: ${state.result.toStringAsPrecision(3)}',
+                    style: const TextStyle(
+                        fontSize: 22, fontWeight: FontWeight.bold),
+                  );
+                } else {
+                  return const Text(
+                    'Something went wrong',
+                    style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 127, 54, 244)),
+                  );
+                }
+              },
+            ),
+          ]),
         ),
       ),
     );
